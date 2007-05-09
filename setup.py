b9d60180819df25fb506396ff82d4cc79d0f2766
Sebastien Pierre <sebastien@type-z.org>
2007-05-09 10:36:12 -0400

    Updated setup.py to support module data Commit description tags:        minor fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 # License           :   Revised BSD License
 # -----------------------------------------------------------------------------
 # Creation date     :   20-Mar-2005
-# Last mod.         :   22-Sep-2006
+# Last mod.         :   08-May-2007
 # -----------------------------------------------------------------------------
 
 import sys ; sys.path.insert(0, "Sources")
@@ -41,6 +41,7 @@ setup(
     url         = "http://www.ivy.fr/tahchee",
     download_url= "http://www.ivy.fr/tahchee/tahchee-%s.tar.gz" % (main.__version__) ,
     package_dir = { "": "Sources" },
+    package_data= { "tahchee.plugins._kiwi": ["*.css"] },
     packages    = ["tahchee", "tahchee.plugins", "tahchee.plugins._kiwi"],
     scripts     = ["Scripts/tahchee"],
     classifiers = [
